6655a4154651c754bc6bf0968a9f8dc5a906a88e
Awaneesh Soni <awaneesh.soni.cer21@iitbhu.ac.in>
2025-02-15 10:36:52 +0530

    domain cors

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ connectDB();
 
 // ✅ Apply CORS Middleware **before** defining routes
 const corsOptions = {
-    origin: ['https://flame-lemon.vercel.app', 'http://localhost:5173'],  
+    origin: ['https://flame-lemon.vercel.app', 'http://localhost:5173', 'https://flame.contentarc.co'],  
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true // ✅ Allow cookies, Authorization headers
